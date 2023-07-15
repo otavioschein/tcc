35d60c19c87563e448db8f7d529b4712d64f0f85
omuck <omuck@grupopanvel.com.br>
2023-07-15 12:16:13 -0300

    Feat: new rasa model with new actions

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -13,21 +13,6 @@ class LivroObj:
         self.biblioteca = biblioteca
         self.urlAcesso = urlAcesso
 
-class ActionListarNomesLivros(Action):
-    def name(self) -> Text:
-        return "action_listar_nomes_livros"
-    
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        response = requests.get("http://localhost:8080/assistant/search/books/names")
-        data = response.json()
-
-        dispatcher.utter_message(text=f"Nomes dos livros: {data}")
-
-        return[]
-
 class ActionBuscarLivroPorTitulo(Action):
     def name(self) -> Text:
         return "action_buscar_livro_por_titulo"
@@ -40,11 +25,16 @@ class ActionBuscarLivroPorTitulo(Action):
 
         responseMinhaBiblioteca = requests.get(f"http://localhost:8080/assistant/books/minha-biblioteca/titulo/{titulo}")
         responsePearson = requests.get(f"http://localhost:8080/assistant/books/pearson-biblioteca/titulo/{titulo}")
+        responseFisica = requests.get(f"http://localhost:8080/assistant/books/biblioteca-fisica/titulo/{titulo}")
 
         dataMinhaBiblioteca = responseMinhaBiblioteca.json()
         livrosMinhaBiblioteca = []
+
         dataPearson = responsePearson.json()
         livrosPearson = []
+
+        dataFisica = responseFisica.json()
+        livrosFisica = []
 
         for livro in dataMinhaBiblioteca:
             livroObj = LivroObj(
@@ -66,24 +56,60 @@ class ActionBuscarLivroPorTitulo(Action):
             )
             livrosPearson.append(livroObj)
 
+        for livro in dataFisica:
+            fisicaObj = LivroObj(
+                id=livro['id'],
+                autor=livro['autor'],
+                titulo=livro['titulo'],
+                biblioteca=livro['biblioteca']
+            )
+            livrosFisica.append(fisicaObj)
+
         message = f"Livros que encontrei para o título {titulo} informado: "
-        messageMinhaBiblioteca = f"Na minha biblioteca: "
-        messagePearson = f"e na biblioteca Pearson: "
-
         dispatcher.utter_message(text=message)
-        dispatcher.utter_message(text=messageMinhaBiblioteca)
 
-        for mbLivro in livrosMinhaBiblioteca:
-            textLivro = f"Titulo {mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
-            dispatcher.utter_message(text=textLivro)
 
-        dispatcher.utter_message(text=messagePearson)
+        if livrosMinhaBiblioteca:
+            messageMinhaBiblioteca = f"Na minha biblioteca: "
 
-        for prLivro in livrosPearson:
-            textLivro = f"Titulo {prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
-            dispatcher.utter_message(text=textLivro)
+            dispatcher.utter_message(text=messageMinhaBiblioteca)
 
-        return [SlotSet("titulo_livro", None)]
+            for mbLivro in livrosMinhaBiblioteca:
+                textLivro = f"Titulo {mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
+                dispatcher.utter_message(text=textLivro)
+        else:
+            messageMinhaBiblioteca = f"Desculpe, na Minha Biblioteca não há livros para esse título."
+            dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+
+        if livrosPearson:
+            messagePearson = f"e na biblioteca Pearson: "
+            dispatcher.utter_message(text=messagePearson)
+            
+            for prLivro in livrosPearson:
+                textLivro = f"Titulo {prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
+                dispatcher.utter_message(text=textLivro)
+        else:
+            messagePearson = f"Desculpe, não foi encontrado nenhum livro com esse nome na biblioteca Pearson"
+            dispatcher.utter_message(messagePearson)
+
+        if livrosFisica:
+            messageFisica = f"Na biblioteca Física: "
+            dispatcher.utter_message(text=messageFisica)
+            
+            for livroFisica in livrosFisica:
+                textLivro = f"Titulo {livroFisica.titulo}, do autor {livroFisica.autor} está disponível"
+                dispatcher.utter_message(text=textLivro)
+        else:
+            messagePearson = f"Desculpe, não foi encontrado nenhum livro com esse nome na biblioteca física"
+            dispatcher.utter_message(messagePearson)
+
+
+        if livrosPearson == [] and livrosMinhaBiblioteca == [] and livrosFisica == []:
+            m = "Verifique se foi escrito corretamente"
+            dispatcher.utter_message(m)
+
+        return[SlotSet("nome_autor", None)]
     
 class ActionBuscarLivroPorAutor(Action):
     def name(self) -> Text:
@@ -97,11 +123,16 @@ class ActionBuscarLivroPorAutor(Action):
 
         responseMinhaBiblioteca = requests.get(f"http://localhost:8080/assistant/books/minha-biblioteca/autor/{autor}")
         responsePearson = requests.get(f"http://localhost:8080/assistant/books/pearson-biblioteca/autor/{autor}")
+        responseFisica = requests.get(f"http://localhost:8080/assistant/books/biblioteca-fisica/autor/{autor}")
 
         dataMinhaBiblioteca = responseMinhaBiblioteca.json()
         livrosMinhaBiblioteca = []
+
         dataPearson = responsePearson.json()
         livrosPearson = []
+
+        dataFisica = responseFisica.json()
+        livrosFisica = []
 
         for livro in dataMinhaBiblioteca:
             livroObj = LivroObj(
@@ -123,24 +154,239 @@ class ActionBuscarLivroPorAutor(Action):
             )
             livrosPearson.append(livroObj)
 
+        for livro in dataFisica:
+            fisicaObj = LivroObj(
+                id=livro['id'],
+                autor=livro['autor'],
+                titulo=livro['titulo'],
+                biblioteca=livro['biblioteca']
+            )
+            livrosFisica.append(fisicaObj)
+
         message = f"Livros que encontrei para o autor {autor} informado: "
-        messageMinhaBiblioteca = f"Na minha biblioteca: "
-        messagePearson = f"e na biblioteca Pearson: "
-
         dispatcher.utter_message(text=message)
-        dispatcher.utter_message(text=messageMinhaBiblioteca)
 
-        for mbLivro in livrosMinhaBiblioteca:
-            textLivro = f"Titulo {mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
-            dispatcher.utter_message(text=textLivro)
 
-        dispatcher.utter_message(text=messagePearson)
+        if livrosMinhaBiblioteca:
+            messageMinhaBiblioteca = f"Na minha biblioteca: "
 
-        for prLivro in livrosPearson:
-            textLivro = f"Titulo {prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
-            dispatcher.utter_message(text=textLivro)
+            dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+            for mbLivro in livrosMinhaBiblioteca:
+                textLivro = f"{mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
+                dispatcher.utter_message(text=textLivro)
+
+        else:
+            messageMinhaBiblioteca = f"Desculpe, na Minha Biblioteca não há livros para esse autor."
+            dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+
+        if livrosPearson:
+            messagePearson = f"E na biblioteca Pearson: "
+            dispatcher.utter_message(text=messagePearson)
+            
+            for prLivro in livrosPearson:
+                textLivro = f"{prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
+                dispatcher.utter_message(text=textLivro)
+        else:
+            messagePearson = f"Desculpe, não foi encontrado nenhum livro para este autor na biblioteca Pearson"
+            dispatcher.utter_message(messagePearson)
         
-        return[SlotSet("nome_autor", None)]
+        if livrosFisica:
+            messageFisica = f"Na biblioteca física: "
+            dispatcher.utter_message(text=messageFisica)
+            
+            for fisicaLivro in livrosFisica:
+                textLivro = f"{fisicaLivro.titulo}, do autor {fisicaLivro.autor} está disponível"
+                dispatcher.utter_message(text=textLivro)
+        else:
+            messagePearson = f"Desculpe, não foi encontrado nenhum livro para este autor na biblioteca física"
+            dispatcher.utter_message(messagePearson)
+
+        if livrosPearson == [] and livrosMinhaBiblioteca == [] and livrosFisica == []:
+            m = "Verifique se foi escrito corretamente"
+            dispatcher.utter_message(m)
+        
+        return[SlotSet("titulo_livro", None)]
+    
+
+class ActionBuscarLivrosSemelhantes(Action):
+    def name(self) -> Text:
+        return "action_buscar_livros_semelhantes"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        titulo = tracker.get_slot("titulo_livro")
+        autor = tracker.get_slot("nome_autor")
+
+        if titulo != None:
+            responseMinhaBiblioteca = requests.get(f"http://localhost:8080/assistant/books/minha-biblioteca/titulo/semelhante/{titulo}")
+            responsePearson = requests.get(f"http://localhost:8080/assistant/books/pearson-biblioteca/titulo/semelhante/{titulo}")
+            responseFisica = requests.get(f"http://localhost:8080/assistant/books/biblioteca-fisica/titulo/semelhante/{titulo}")
+
+            dataMinhaBiblioteca = responseMinhaBiblioteca.json()
+            livrosMinhaBiblioteca = []
+            dataPearson = responsePearson.json()
+            livrosPearson = []
+            dataFisica = responseFisica.json()
+            livrosFisica = []
+
+            for livro in dataMinhaBiblioteca:
+                livroObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca'],
+                    urlAcesso=livro['urlAcesso']
+                )
+                livrosMinhaBiblioteca.append(livroObj)
+
+            for livro in dataPearson:
+                livroObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca'],
+                    urlAcesso=livro['urlAcesso']
+                )
+                livrosPearson.append(livroObj)
+
+            for livro in dataFisica:
+                fisicaObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca']
+                )
+                livrosFisica.append(fisicaObj)
+
+            message = f"Livros semelhantes para {titulo}: "
+            dispatcher.utter_message(text=message)
+
+            if livrosMinhaBiblioteca:
+                messageMinhaBiblioteca = f"Na minha biblioteca: "
+
+                dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+                for mbLivro in livrosMinhaBiblioteca:
+                    textLivro = f"{mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
+                    dispatcher.utter_message(text=textLivro)
+            else:
+                messageMinhaBiblioteca = f"Desculpe, na Minha Biblioteca não há livros para esse título."
+                dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+            if livrosPearson:
+                messagePearson = f"e na biblioteca Pearson: "
+                dispatcher.utter_message(text=messagePearson)
+                
+                for prLivro in livrosPearson:
+                    textLivro = f"{prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
+                    dispatcher.utter_message(text=textLivro)
+            else:
+                messagePearson = f"Desculpe, não foi encontrado nenhum livro com esse nome na biblioteca Pearson"
+                dispatcher.utter_message(messagePearson)
+
+            if livrosFisica:
+                messageFisica = f"Na biblioteca física: "
+                dispatcher.utter_message(text=messageFisica)
+                
+                for fisicaLivro in livrosFisica:
+                    textLivro = f"{fisicaLivro.titulo}, do autor {fisicaLivro.autor} está disponível"
+                    dispatcher.utter_message(text=textLivro)
+            else:
+                messagePearson = f"Desculpe, não foi encontrado nenhum livro na biblioteca física"
+                dispatcher.utter_message(messagePearson)
+
+            return []
+        
+        else:
+            responseMinhaBiblioteca = requests.get(f"http://localhost:8080/assistant/books/minha-biblioteca/autor/semelhante/{autor}")
+            responsePearson = requests.get(f"http://localhost:8080/assistant/books/pearson-biblioteca/autor/semelhante/{autor}")
+            responseFisica = requests.get(f"http://localhost:8080/assistant/books/biblioteca-fisica/autor/semelhante/{autor}")
+
+            dataMinhaBiblioteca = responseMinhaBiblioteca.json()
+            livrosMinhaBiblioteca = []
+            dataPearson = responsePearson.json()
+            livrosPearson = []
+            dataFisica = responseFisica.json()
+            livrosFisica = []
+
+            for livro in dataMinhaBiblioteca:
+                livroObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca'],
+                    urlAcesso=livro['urlAcesso']
+                )
+                livrosMinhaBiblioteca.append(livroObj)
+
+            for livro in dataPearson:
+                livroObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca'],
+                    urlAcesso=livro['urlAcesso']
+                )
+                livrosPearson.append(livroObj)
+
+            for livro in dataFisica:
+                fisicaObj = LivroObj(
+                    id=livro['id'],
+                    autor=livro['autor'],
+                    titulo=livro['titulo'],
+                    biblioteca=livro['biblioteca']
+                )
+                livrosFisica.append(fisicaObj)
+
+            message = f"Livros semelhantes para o autor {autor}: "
+            dispatcher.utter_message(text=message)
+
+
+            if livrosMinhaBiblioteca:
+                messageMinhaBiblioteca = f"Na minha biblioteca: "
+
+                dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+                for mbLivro in livrosMinhaBiblioteca:
+                    textLivro = f"{mbLivro.titulo}, do autor {mbLivro.autor} e está disponível no link {mbLivro.urlAcesso}"
+                    dispatcher.utter_message(text=textLivro)
+
+            else:
+                messageMinhaBiblioteca = f"Desculpe, na Minha Biblioteca não há livros para esse autor."
+                dispatcher.utter_message(text=messageMinhaBiblioteca)
+
+
+            if livrosPearson:
+                messagePearson = f"E na biblioteca Pearson: "
+                dispatcher.utter_message(text=messagePearson)
+                
+                for prLivro in livrosPearson:
+                    textLivro = f"{prLivro.titulo}, do autor {prLivro.autor} e está disponível no link {prLivro.urlAcesso}"
+                    dispatcher.utter_message(text=textLivro)
+            else:
+                messagePearson = f"Desculpe, não foi encontrado nenhum livro para este autor na biblioteca Pearson"
+                dispatcher.utter_message(messagePearson)
+            
+            if livrosFisica:
+                messageFisica = f"Na biblioteca física: "
+                dispatcher.utter_message(text=messageFisica)
+                
+                for fisicaLivro in livrosFisica:
+                    textLivro = f"{fisicaLivro.titulo}, do autor {fisicaLivro.autor} está disponível"
+                    dispatcher.utter_message(text=textLivro)
+            else:
+                messagePearson = f"Desculpe, não foi encontrado nenhum livro na biblioteca física"
+                dispatcher.utter_message(messagePearson)
+
+            if livrosPearson == [] and livrosMinhaBiblioteca == []:
+                m = "Verifique se foi escrito corretamente"
+                dispatcher.utter_message(m)
+            
+            return[]
 
 class ActionExtrairNomeDeLivroNoInput(Action):
     def name(self) -> Text:
@@ -162,6 +408,7 @@ class ActionExtrairNomeDeLivroNoInput(Action):
         else:
             dispatcher.utter_message("Desculpe, não entendi.")
         
+
 class ActionExtrairNomeDeAutorNoInput(Action):
     def name(self) -> Text:
         return "action_extrair_nome_de_autor_no_input"
